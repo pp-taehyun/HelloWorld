0f9b0a88ca19c97af8c5c9b1295dc8d74d320fcd
taehyun <taehyun@piedpixels.com>
2024-11-05 14:44:40 +0900

    [발키리/한태현] SerializationJson 추가

---
+++ Server/Controller/JsonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Server.Controller
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class JsonController : ControllerBase
+    {
+        class ResponseData
+        {
+            public required string message { get; set; }
+        }
+
+        [HttpGet]
+        public string SerializeJson()
+        {
+            var data = new ResponseData { message = "Hello, World!" };
+            var json = JsonSerializer.Serialize(data);
+
+            Response.ContentType = "application/json";
+            Response.ContentLength = json.Length;
+
+            return json;
+        }
+    }
+}
