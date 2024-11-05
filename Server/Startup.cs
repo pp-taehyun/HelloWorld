7bfb7fa36ecc92c7046e3a64a43b785bb79651c1
taehyun <taehyun@piedpixels.com>
2024-11-05 12:40:56 +0900

    Change to Mvc

---
+++ Server/Startup.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+namespace Server
+{
+    class Startup
+    {
+        public IConfiguration Configuration { get; init; }
+
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
