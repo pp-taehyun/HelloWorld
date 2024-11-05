7c67d7f469f20c94e4c647695dd8e0a3801872dd
taehyun <taehyun@piedpixels.com>
2024-11-05 12:40:56 +0900

    [발키리/한태현] Mvc로 변경

---
+++ Server/Controller/PlainController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server;
+
+namespace JsonSerialization.Controller
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PlainController : ControllerBase
+    {
+        [HttpGet]
+        public string Plaintext()
+        {
+            var HelloWorld = "Hello, World!";
+            return HelloWorld;
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,18 @@
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+        }
+    }
+}
