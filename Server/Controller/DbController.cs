74d81f848223962fc08568457330d587b7442afa
taehyun <taehyun@piedpixels.com>
2024-11-05 18:39:35 +0900

    [발키리/한태현] Single Query api 추가

--- Server/Controller/DbController.cs
+++ Server/Controller/DbController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Server.Main;
+using System.Text.Json;
 
 namespace Server.Controller
 {
@@ -6,10 +9,22 @@ namespace Server.Controller
     [ApiController]
     public class DbController : ControllerBase
     {
-        //[HttpGet]
-        //public IActionResult ExecuteSingleQuery()
-        //{
-        //    Response.ContentType = "application/json";
-        //}
+        [HttpGet]
+        public IActionResult ExecuteSingleQuery()
+        {
+            Random random = new Random();
+            Database.World selectedWorld = Program.Connection!.Query<Database.World>("SELECT * FROM World WHERE Id = @Id",
+                                                            new Database.World()
+                                                            {
+                                                                Id = random.Next(0, 10_000) + 1
+                                                            }).First();
+
+            string json = JsonSerializer.Serialize(selectedWorld);
+
+            Response.ContentType = "application/json";
+            Response.ContentLength = json.Length;
+
+            return Content(json);
+        }
     }
 }
