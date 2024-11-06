8f3d0c9b8ee80236828be65602d274cce84e3f1c
taehyun <taehyun@piedpixels.com>
2024-11-06 15:06:56 +0900

    [발키리/한태현] /updates api 기능 구현

--- Server/Controller/UpdatesController.cs
+++ Server/Controller/UpdatesController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Server.Database;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text.Json;
 
 namespace Server.Controller
 {
@@ -6,10 +10,38 @@ namespace Server.Controller
     [ApiController]
     public class UpdatesController : ControllerBase
     {
-        //[HttpGet]
-        //public async Task<IActionResult> UpdateData()
-        //{
+        private static Random Random { get; } = new();
 
-        //}
+        [HttpGet]
+        public IActionResult UpdateData(int queries)
+        {
+            queries = Math.Clamp(queries, 1, 500);
+
+            var worldList = new List<World>();
+
+            var sql = "UPDATE World SET RandomNumber = @RandomNumber WHERE Id = @Id";
+
+            for (var i = 0; i < queries; i++)
+            {
+                int selectId = Random.Next(0, 10_000) + 1;
+                int newNumber = Random.Next(0, 10_000) + 1;
+
+                var world = new World
+                {
+                    id = selectId,
+                    randomNumber = newNumber
+                };
+                worldList.Add(world);
+
+                DatabaseManager.Connection.Query<World>(sql, world);
+            }
+
+            string json = JsonSerializer.Serialize(worldList);
+
+            Response.ContentType = "application/json";
+            Response.ContentLength = json.Length;
+
+            return Content(json);
+        }
     }
 }
